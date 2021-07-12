7bac18a848017c7c5d8370671cb56aadcc78c1c8
unitedscalearts <83475839+unitedscalearts@users.noreply.github.com>
2021-07-12 11:18:03 -0300

    Update prueba_pasoapaso.ino

--- prueba_pasoapaso/prueba_pasoapaso.ino
+++ prueba_pasoapaso/prueba_pasoapaso.ino
@@ -41,10 +41,10 @@ boolean fStep2 [4] = {LOW, LOW, HIGH, HIGH};
 boolean fStep3 [4] = {HIGH, LOW, LOW, HIGH};
 boolean fStep4 [4] = {LOW, HIGH, HIGH, LOW};
 uint8_t fPos = 0;
-boolean hStep1 [8] = {HIGH, HIGH, LOW, LOW, LOW, LOW, LOW, LOW};
-boolean hStep2 [8] = {LOW, LOW, LOW, HIGH, HIGH, HIGH, LOW, HIGH};
-boolean hStep3 [8] = {LOW, HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW};
-boolean hStep4 [8] = {LOW, LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH};
+boolean hStep1 [8] = {HIGH, HIGH, LOW,  LOW,  LOW,  LOW,  LOW,  HIGH};
+boolean hStep2 [8] = {LOW,  LOW,  LOW,  HIGH, HIGH, HIGH, LOW,  LOW};
+boolean hStep3 [8] = {LOW,  HIGH, HIGH, HIGH, LOW,  LOW,  LOW,  LOW};
+boolean hStep4 [8] = {LOW,  LOW,  LOW,  LOW,  LOW,  HIGH, HIGH, HIGH};
 
 void motorStep() {
   digitalWrite(OUT1, fStep1[fPos]);
@@ -52,5 +52,11 @@ void motorStep() {
   digitalWrite(OUT3, fStep3[fPos]);
   digitalWrite(OUT4, fStep4[fPos]);
   fPos++;
-  fPos%=4;
+  fPos%=4;/*
+  digitalWrite(OUT1, hStep1[fPos]);
+  digitalWrite(OUT2, hStep2[fPos]);
+  digitalWrite(OUT3, hStep3[fPos]);
+  digitalWrite(OUT4, hStep4[fPos]);
+  fPos++;
+  fPos%=8;*/
 }
