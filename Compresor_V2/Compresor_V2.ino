c3fe194babf2bdc04b791af77d7480a165ab33a8
unitedscalearts <83475839+unitedscalearts@users.noreply.github.com>
2021-08-11 14:42:49 -0300

    Add lcd y cambio de pines
    Maquina de estados finalizada, cambio de algunos pines digitales a analogicos y otros, para poder agregar lcd y sd en un Arduino UNO. LCD 16x2 (con control de backlight) y programa de compresor funcionando correctamente, con boton reset y service, 2 leds y una llave.

--- Compresor_V2/Compresor_V2.ino
+++ Compresor_V2/Compresor_V2.ino
@@ -9,6 +9,7 @@
 
 #include <SPI.h>
 #include <SD.h>
+#include <LiquidCrystal.h>
 
 File myFile;
 uint32_t data = 0;
@@ -20,27 +21,29 @@ uint32_t data = 0;
  ** CS - pin 4
  */
 
-
+const int rs = 10, en = 9, d4 = 8, d5 = 7, d6 = 6, d7 = 5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Entradas
-#define BUTTON_SERVICE      10    // Activo Alto?
+#define BUTTON_SERVICE      A3
 #define SERVICE_PRESIONADO  digitalRead(BUTTON_SERVICE)
-#define LLAVE               9
+#define LLAVE               A1
 #define LLAVE_ON            digitalRead(LLAVE)
 #define LLAVE_OFF           !digitalRead(LLAVE)
-#define BUTTON_RESET        8
+#define BUTTON_RESET        A2
 #define RESET_PRESIONADO    digitalRead(BUTTON_RESET)
-#define S_MOTOR             7
+#define S_MOTOR             A0
 #define MOTOR_ANDANDO       !digitalRead(S_MOTOR)
 
 // Salidas
-#define CONTACTOR       5
-#define LED_OFF         3
-#define LED_ON          2
+#define BACKLIGHT       3
+#define CONTACTOR       2
+#define LED_OFF         1
+#define LED_ON          0
 
 // Tiempos
-#define STOP_TIMEOUT    600    //60000 (10 min) (1 - 10ms) Tiempo maximo en funcionamiento continuo
-#define SERVICE_TIMEOUT 1080  //1080000 (30 hs) (1 - 10ms) Tiempo maximo de funcionamiento
+#define STOP_TIMEOUT    6000    //60000 (10 min) (1 - 10ms) Tiempo maximo en funcionamiento continuo
+#define SERVICE_TIMEOUT 10800  //1080000 (30 hs) (1 - 10ms) Tiempo maximo de funcionamiento
 
 // Variables de estado
 #define INICIO          0
@@ -50,13 +53,23 @@ uint32_t data = 0;
 #define SERVICE         4
 
 // Variables Globales
-uint8_t estado = REPOSO;
-boolean timer_flag = false;
+uint8_t estado = INICIO;
+volatile boolean timer_flag = false;
+uint32_t timer_count = 0;
+boolean timer_display_flag = true;
+uint32_t timer_display_count = 0;
 unsigned long previousMillis = 0;
 const long interval = 10;
+#define DISPLAY_REFRESH 1000/interval
+boolean service_flag = false;
+uint32_t service_delay = 0;
+
+void timer2_init();
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Iniciando... ");
   pinMode(BUTTON_SERVICE, INPUT);
   pinMode(LLAVE, INPUT);
   pinMode(BUTTON_RESET, INPUT);
@@ -64,14 +77,14 @@ void setup() {
   pinMode(LED_OFF, OUTPUT);
   pinMode(LED_ON, OUTPUT);
   pinMode(CONTACTOR, OUTPUT);
+  pinMode(BACKLIGHT, OUTPUT);
+  digitalWrite(BACKLIGHT, HIGH);
 }
 
 
 void loop() {
   softTimer();
   update_estado();
-  Serial.print("Estado: ");
-  Serial.println(estado);
 }
 
 
@@ -80,25 +93,34 @@ void softTimer() {
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     timer_flag = true;
+    timer_display_count++;
+    if (timer_display_count == DISPLAY_REFRESH) {
+      timer_display_count = 0;
+      timer_display_flag = true;
+    }
+    if(service_flag) {
+      if(service_delay) service_delay--;
+    }
   }
 }
 
 
-uint32_t motorServiceCount = 0;
-
 void update_estado() {
   static uint16_t ledCount = 0;
   static uint32_t motorCount = 0;
+  static uint32_t motorServiceCount = 0;
   if (!timer_flag) return;
-  timer_flag = true;
+  timer_flag = false;
   
   switch(estado) {
 
     case INICIO:
       motorCount = 0;
       estado = REPOSO;
+      lcd.clear();
       break;
 
+    // Compresor andando normalmente, contando el tiempo de service y controlando que no supere el tiempo maximo de funcionamiento continuo
     case REPOSO:
       if (LLAVE_OFF) estado = OFF;
       else if (motorServiceCount >= SERVICE_TIMEOUT) estado = SERVICE;
@@ -112,13 +134,32 @@ void update_estado() {
         digitalWrite(CONTACTOR, HIGH);
         digitalWrite(LED_OFF, LOW);
         digitalWrite(LED_ON, HIGH);
+      }      
+      if(!timer_display_flag) break;
+      timer_display_flag = false;
+      lcd.setCursor(0, 0);
+      lcd.print("Motor ");
+      if(!motorCount) lcd.print("OFF");
+      else {
+        lcd.print("ON ");
+        lcd.print((uint32_t)(motorCount/6000));
+        lcd.print("m ");
+        lcd.print((uint32_t)((motorCount%6000)/100));
+        lcd.print("s");
       }
-      Serial.print("Motor Count: ");
-      Serial.println(motorCount);
-      Serial.print(" Motor Sv: ");
-      Serial.println(motorServiceCount);
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print("Serv ");
+      lcd.print((uint32_t)((SERVICE_TIMEOUT-motorServiceCount)/360000));
+      lcd.print("h ");
+      lcd.print((uint32_t)(((SERVICE_TIMEOUT-motorServiceCount)%360000)/6000));
+      lcd.print("m ");
+      lcd.print((uint32_t)(((SERVICE_TIMEOUT-motorServiceCount)%6000)/100));
+      lcd.print("s");
+      lcd.print("                ");
       break;
 
+   // Si el compresor supero el tiempo maximo de funcionamiento continuo
    case STOP:
       digitalWrite(CONTACTOR, LOW);
       digitalWrite(LED_OFF, LOW);
@@ -132,32 +173,79 @@ void update_estado() {
         motorCount = 0;
         estado = REPOSO;
       }
+      lcd.setCursor(0, 0);
+      lcd.print("STOP, precaucion");
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print("Esperando reset!");
+      lcd.print("                ");
       break;
 
+   // Si el compresor supero el tiempo maximo de funcionamiento
    case SERVICE:
+      static boolean text_flash = false;
       digitalWrite(CONTACTOR, LOW);
       ledCount++;
       if(ledCount == 50) {
         ledCount = 0;
         digitalWrite(LED_OFF, !digitalRead(LED_OFF));
         digitalWrite(LED_ON, !digitalRead(LED_ON));
+        text_flash = !text_flash;
       }
       if (LLAVE_OFF) estado = OFF;
-      // falta cambiar de estado
+      if(text_flash) {
+        digitalWrite(BACKLIGHT, LOW);
+        lcd.clear();
+        break;
+      }
+      digitalWrite(BACKLIGHT, HIGH);
+      lcd.setCursor(0, 0);
+      lcd.print("NECESITA SERVICE");
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print("Call Mechatronic");
+      lcd.print("                ");
       break;
 
+   // Si la llave esta apagada
    case OFF:
+      //digitalWrite(BACKLIGHT, LOW);
+      analogWrite(BACKLIGHT,10);
       digitalWrite(CONTACTOR, LOW);
       digitalWrite(LED_OFF, HIGH);
       digitalWrite(LED_ON, LOW);
       if(LLAVE_ON) {
         motorCount = 0;
+        digitalWrite(BACKLIGHT, HIGH);
         estado = REPOSO;
       }
+      lcd.setCursor(0, 0);
+      lcd.print("APAGADO");
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
       break;
 
    default:
       estado = INICIO;
       break;
+  }
+
+  // Debounce con delay de boton de servicio
+  if(SERVICE_PRESIONADO && !service_flag) {
+    service_flag = true;
+    service_delay = 2000/interval;
+  }
+  else if (SERVICE_PRESIONADO && service_flag && !service_delay) {
+    estado = INICIO;
+    motorCount = 0;
+    motorServiceCount = 0;
+    digitalWrite(BACKLIGHT, HIGH);
+    digitalWrite(CONTACTOR, LOW);
+    digitalWrite(LED_OFF, HIGH);
+    digitalWrite(LED_ON, HIGH);
+  }
+  if (!SERVICE_PRESIONADO && service_flag) {
+    service_flag = false;
   }
 }
